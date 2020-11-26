53800201ebb2a787c21add7f01639811709de48b
madgb <madgb00@gmail.com>
2020-11-26 12:14:49 -0500

    v5

---
+++ precache-manifest.ada392b2142c0980539dbc5c83a409ae.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "344e8a86049c0d36823ff6907f27c035",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bd4bdff09ab945af1872",
+    "url": "./static/css/main.b865452b.chunk.css"
+  },
+  {
+    "revision": "a8d8492b029dd1c2bf3b",
+    "url": "./static/js/2.bb788535.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "./static/js/2.bb788535.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd4bdff09ab945af1872",
+    "url": "./static/js/main.accf9319.chunk.js"
+  },
+  {
+    "revision": "dbab036f87ca1350243a",
+    "url": "./static/js/runtime-main.851de3a6.js"
+  },
+  {
+    "revision": "e67e358c72e30a5ec593534154b91ea6",
+    "url": "./static/media/avatar.e67e358c.png"
+  },
+  {
+    "revision": "08ca08ecff8a45de139f873bdf0e0823",
+    "url": "./static/media/dna-background.08ca08ec.jpg"
+  },
+  {
+    "revision": "a8571a6e957f78e80e9a308a7aa8196e",
+    "url": "./static/media/femaleAvatar.a8571a6e.png"
+  },
+  {
+    "revision": "b56d27278e98641a737400ad0b366481",
+    "url": "./static/media/loadingGif.b56d2727.gif"
+  },
+  {
+    "revision": "a625b3fa179243b59651bb628fda8900",
+    "url": "./static/media/logo.a625b3fa.png"
+  }
+]);
